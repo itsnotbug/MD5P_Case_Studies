db4466f70a0a814131a294e98f42dac5701d676c
Bernadette <bernadettedejesus99@gmail.com>
2023-05-21 22:30:32 +0800

    updated into cleaner code - as per RC's review

--- src/main/kotlin/onlinebookstore/ShoppingCart.kt
+++ src/main/kotlin/onlinebookstore/ShoppingCart.kt
@@ -4,37 +4,37 @@ class ShoppingCart{
     //5. Shopping Cart:
     //- Create a class `ShoppingCart` that represents the shopping cart for a user.
     //- Include functions to add books to the cart (`addToCart()`), remove books from the cart (`removeFromCart()`), and display the cart contents (`displayCart()`).
+    private val bookList = mutableListOf<Book>()
+    private val title :String
+    private val author:String
+    private val price :Double
+    private val quantityInStock:Int
 
-    private val title = mutableListOf<String>()
-    private val author = mutableListOf<String>()
-    private val price = mutableListOf<Double>()
-    private val quantityInStock= mutableListOf<Int>()
-    private var ctr=0
+    init{
+        title=""
+        author=""
+        price=0.0
+        quantityInStock=0
+    }
     fun addToCart(book:Book) {
-        title.add(book.getTitle())
-        author.add(book.getAuthor())
-        price.add(book.getPrice())
-        quantityInStock.add(book.getQuantityInStocks())
+        bookList.add(book)
         println("Book added to cart: ${book.getTitle()}")
     }
     fun removeFromCart(book: Book) {
-        title.remove(book.getTitle())
-        author.remove(book.getAuthor())
-        price.remove(book.getPrice())
-        quantityInStock.remove(book.getQuantityInStocks())
+        bookList.remove(book)
         println("Book removed from cart: ${book.getTitle()}")
 
     }
     fun displayCart() {
-        while (ctr<title.size && ctr<author.size && ctr<price.size && ctr<quantityInStock.size){
+        for (book in bookList){
             println("Shopping Cart: ")
-            println("Title: ${title[ctr]} ")
-            println("Author: ${author[ctr]}")
-            println("Price: ${price[ctr]}")
-            println("Quantity in stock: ${quantityInStock[ctr]} ")
+            println("Title: ${book.getTitle()} ")
+            println("Author: ${book.getAuthor()}")
+            println("Price: ${book.getPrice()}")
+            println("Quantity in stock: ${book.getQuantityInStocks()} ")
             println(" ------------------")
-            ctr++
         }
+
 
     }
 
